c470c36962b66db10f75e07acbf001c451844bba
dandaluga <dan@daluga.com>
2017-01-29 19:47:54 -0600

    Initial commit

---
+++ CheckWebsiteWithLWP.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+use CGI qw( :standard);
+
+my $query = CGI->new;
+
+require LWP::UserAgent;
+
+my $host = $query->param('hostname');
+#my $port = $query->param('port');
+my $timeout = $query->param('timeout');
+
+my $ua = LWP::UserAgent->new;
+
+$ua->timeout($timeout);
+
+my $response = $ua->get($host);
+
+print "Content-Type: text/html \n\n";
+
+if ($response->is_success) {
+    #print $response->content;
+    print "Connection established for: $host";
+} else {
+    print "Connection failed for: $host";
+    print "<br/>";
+    print "Connection failed for the following reason: " . $response->status_line;
+    #print $response->status_line;
+}
