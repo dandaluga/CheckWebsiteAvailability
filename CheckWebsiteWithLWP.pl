6d0ec811a95885137f4a78b0d71167259fffbd9e
Dan DaLuga <dan@daluga.com>
2017-02-15 22:05:58 -0600

    A round of changes to display the redirects.

--- CheckWebsiteWithLWP.pl
+++ CheckWebsiteWithLWP.pl
@@ -26,6 +26,35 @@ if ($response->is_success) {
     print "Connection established for: $host";
     print "<br/>";
     print "Connection status is: " . $response->status_line;
+    print "<br/>";
+    #print "HTTP content is " . $response->decoded_content;
+    if ($response->previous) {
+        print "<br/>";
+        print "Redirect Summary: ";
+        print "<br/>";
+        #print "Host " . $host . " redirected to " . $response->request->uri;
+        #print "Host " . $host . " redirected to ";
+        my $found_an_https_redirect = 0;
+        foreach my $url ($response->redirects) {
+            print "<br/>" ;
+            #print "Redirect -> " . $url->base;
+            print "Redirect -> " . $url->request->uri;
+            #print "Redirect -> " . $url->request->uri . " the HTTP status is " . $url->code;
+            if (index($url->request->uri, "https://") != -1) {
+                $found_an_https_redirect = 1;
+            }
+        };
+        print "<br/>";
+        print "<br/>";
+        print "Final Destination -> " . $response->request->uri . "the HTTP statis is " . $response->code;
+        print "<br/>";
+        print "<br/>";
+        if ($found_an_https_redirect) {
+            print "Found an https redirect!";
+        } else {
+            print "Did NOT find an https redirect!";
+        }
+    }
 } else {
     print "Connection failed for: $host";
     print "<br/>";
